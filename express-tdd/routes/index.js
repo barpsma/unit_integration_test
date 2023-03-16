8c3410db830c474bc2554f6d1a18d05dc46d248b
akbar <barmuhammad44@gmail.com>
2023-03-16 19:30:51 +0700

    unit & integration test

---
+++ express-tdd/routes/index.js
@@ -0,0 +1,10 @@
+// routes/index.js
+var express = require('express');
+var router = express.Router();
+
+const baseController = require('../controllers/baseController');
+
+router.get('/', baseController.hello);
+router.get('/sum', baseController.sum);
+
+module.exports = router;
